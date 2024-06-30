21f50ac655d6b64ba4a947a36e9a67ee55bf7779
kamrankhan361 <kamrankhan3.61k@gmail.com>
2024-06-30 22:20:28 +0500

    first commit

---
+++ Gigaland-Redux-CMS-v4/gigaland-retro/src/core/api.js
@@ -0,0 +1,21 @@
+const api = {
+    // baseUrl: 'http://localhost:1337',
+    baseUrl: 'http://174.138.31.155:1337',
+    nfts: '/api/nfts',
+    nftShowcases: '/api/nft_showcases',
+    authors: '/api/authors',
+    authorsSales: '/api/author_ranks',
+    hotCollections: '/api/hot-collections',
+    contactUs: '/api/contact-forms',
+    blogs: '/api/blog-posts',
+    recent: '/api/blog-posts/posts/recents',
+    comments: '/api/blog-posts/comments',
+    tags: '/api/blog-posts/tags',
+}
+
+export const openseaApi = {
+    base: 'https://testnets.opensea.io',
+    api: 'https://testnets-api.opensea.io',
+}
+
+export default api;
---
+++ Gigaland-Redux/gigaland-retro/config-overrides.js
@@ -0,0 +1,73 @@
+const webpack = require('webpack'); 
+module.exports = function override(config) { 
+    const fallback = config.resolve.fallback || {}; 
+    Object.assign(fallback, { 
+        "crypto": require.resolve("crypto-browserify"), 
+        "stream": require.resolve("stream-browserify"), 
+        "assert": require.resolve("assert"), 
+        "http": require.resolve("stream-http"), 
+        "https": require.resolve("https-browserify"), 
+        "os": require.resolve("os-browserify"), 
+        "url": require.resolve("url"),
+        "path": require.resolve("path-browserify"), 
+        "fs": false,
+    }) 
+
+    config.ignoreWarnings = [
+        {
+            module: /node_modules\/wyvern-schemas/
+        },
+        {
+            module: /node_modules\/wyvern-js/
+        },
+        {
+            module: /node_modules\/web3/
+        },
+        {
+            module: /node_modules\/opensea-js/
+        },
+        {
+            module: /node_modules\/ethereumjs-vm/
+        },
+        {
+            module: /node_modules\/ethereumjs-tx/
+        },
+        {
+            module: /node_modules\/ethereumjs-blockchain/
+        },
+        {
+            module: /node_modules\/ethereumjs-account/
+        },
+        {
+            module: /node_modules\/ethereumjs-common/
+        },
+        {
+            module: /node_modules\/ethereum-types/
+        },
+        {
+            module: /node_modules\/@0x\/utils/
+        },
+        {
+            module: /node_modules\/@0x\/json-schemas/
+        },
+        {
+            module: /node_modules\/@0x\/base-contract/
+        },
+        {
+            module: /node_modules\/@0x\/assert/
+        },
+        {
+            module: /node_modules\/@opensea/
+        },
+    ];
+    
+    config.resolve.fallback = fallback; 
+    config.plugins = (config.plugins || []).concat([ 
+        new webpack.ProvidePlugin({ 
+            process: 'process/browser', 
+            Buffer: ['buffer', 'Buffer'] 
+        }) 
+    ])
+
+    return config; 
+}
